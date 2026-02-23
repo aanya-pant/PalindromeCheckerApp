80e236262a8163340593f046d7addb41f43fe0ac
Aanya <pantaanya2402@gmail.com>
2026-02-23 15:56:35 +0530

    Aanya Added UC2

--- untitled/src/PalindromeCheckerApp.java
+++ untitled/src/PalindromeCheckerApp.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main (String[] args){
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        boolean isPalindrome = true;
         System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+        System.out.print("Input Text : ");
+        String text = sc.nextLine();
+        for (int i = 0; i<text.length()/2; i++ ){
+            if (text.charAt(i)!=text.charAt(text.length()-1-i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome) {
+            System.out.println(text + " is a palindrome");
+        }else{
+            System.out.println(text + " is not a palindrome");
+        }
     }
 }
