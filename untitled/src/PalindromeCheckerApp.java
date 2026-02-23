124ab9aeeeaf77543c68cc6e4f335a615abf89bc
Aanya <pantaanya2402@gmail.com>
2026-02-23 16:17:57 +0530

    Aanya Added UC3

--- untitled/src/PalindromeCheckerApp.java
+++ untitled/src/PalindromeCheckerApp.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Enter text : ");
+        String text = sc.nextLine();
+        String reversedText = "";
+        for (int i = text.length() - 1; i>=0; i--)
+            reversedText += text.charAt(i);
+        if(text.equalsIgnoreCase(reversedText))
+            System.out.println(text + " is a palindrome");
+        else
+            System.out.println(text + " is not a palindrome");
     }
 }
