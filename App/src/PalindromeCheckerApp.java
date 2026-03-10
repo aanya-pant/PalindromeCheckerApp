d5a63ada4cb3502e38c4b4008d4e9eb460286bf8
DivyeBatwara <divye.batwara@gmail.com>
2026-03-10 15:32:56 +0530

    UC7

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,18 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
         for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+            deque.addLast(c);
+
         }
         boolean isPalindrome = true;
-        while (!queue.isEmpty()){
-            if(queue.remove()!= stack.pop()){
+        while (deque.size()>1){
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+            if(first!=last){
                 isPalindrome = false;
                 break;
             }
